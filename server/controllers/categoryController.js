011bcaba4ab46b36c4b57ed7189bde49b0b265b4
manankumar94 <mortal9193@gmail.com>
2024-09-27 17:24:49 +0530

    Category_Actions

---
+++ server/controllers/categoryController.js
@@ -0,0 +1,49 @@
+import categoryModel from "../models/categoryModel.js";
+
+class CategoryController{
+    static  getAllCategories = async (req, res)=>{
+       try {
+            const fetchAllCategories= await categoryModel.find({});
+            return res
+                .status(200)
+                .json(fetchAllCategories);
+       } catch (error) {
+        return res
+            .status(400)
+            .json({message: error.message});
+       }
+    }
+    static addNewcategory= async (req, res)=>{
+        const {title}= req.body;
+        try {
+            if(title){
+                const newcategory= new categoryModel({
+                    title,
+                })
+
+                const savedCategory= await newcategory.save();
+                if(savedCategory){
+                    return res
+                        .status(200)
+                        .json({
+                            CategoryId:savedCategory._id,
+                            message: "Category Added Successfully"});
+                } else{
+                    return res
+                    .status(400)
+                    .json({message: "Category Not Saved"});
+                }
+            } else{
+                return res
+                    .status(400)
+                    .json({message: "Title is Required"});
+            }
+        } catch (error) {
+            return res
+                .status(400)
+                .json({message: error.message});
+        }
+    }
+}
+
+export default CategoryController;
