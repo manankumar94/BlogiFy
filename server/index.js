ea26802e07b31d82b12b10fc62db4369cd846409
manankumar94 <mortal9193@gmail.com>
2024-09-27 17:34:01 +0530

    Backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import cors from "cors";
+import connectToMongo from "./config/db.js";
+import router from "./routes/blog.js";
+
 const app= express();
 
 const PORT=9000;
+
+connectToMongo();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
 
 // for testing purpose only
 app.get("/",(req, res)=>{
@@ -9,6 +19,9 @@ app.get("/",(req, res)=>{
      .status(200)
      .send("For testing Purpose Only");
 })
+
+// API Routes
+app.use("/api", router);
 
 app.listen(PORT, (err)=>{
     if(err) console.log(err);
