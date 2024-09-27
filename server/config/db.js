fcfe063c750aa933b47925f636088c79786c0c7b
manankumar94 <mortal9193@gmail.com>
2024-09-27 11:45:15 +0530

    Database

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,10 @@
- // isnqdsws cfejfe ffejbffujebfjebjenlkdmvkl;rn jkenfkenf ejhfhkenjfklefdfm 
+import mongoose from "mongoose";
+
+const connectToMongo= async () => {
+    const res= await mongoose.connect("mongodb://localhost:27017/Blog_App");
+    if(res){
+        console.log("Connected Successfully to Database (Blog_App)");
+    }
+}
+
+export default connectToMongo;
